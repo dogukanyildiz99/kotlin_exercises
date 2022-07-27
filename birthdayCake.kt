3f2f1644619086114f0a057c3a9863d1b850290a
Doğukan YILDIZ <56637126+dogukanyildiz99@users.noreply.github.com>
2022-07-27 14:35:10 +0300

    Create birthdayCake.kt

---
+++ birthdayCake.kt
@@ -0,0 +1,52 @@
+fun main() {
+
+    var age = 24
+    var layers = 5
+    
+    printCakeCandles(age)
+    printCakeTop(age)
+    printCakeBottom(age, layers)
+
+}
+
+fun printCakeCandles(age: Int) {
+    //flames
+    print(" ")
+    repeat(age){
+        print(",")
+    }
+    println()
+    //candles
+    print(" ")
+    repeat(age){
+    	print("|")
+   	}
+    println()
+}
+
+fun printCakeTop(age: Int) {
+    repeat(age+2){
+        print("=")
+    }
+    println()
+}
+
+fun printCakeBottom(age: Int, layers: Int) {
+    repeat(layers){
+        repeat(age+2){
+            print("@")
+    	}
+    	println()
+    }
+}
+
+/* Output
+ ,,,,,,,,,,,,,,,,,,,,,,,,
+ ||||||||||||||||||||||||
+==========================
+@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@
+*/
