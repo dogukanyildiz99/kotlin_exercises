307bcf3ac4990211744c28f181bbb2b656af74e5
Doğukan YILDIZ <56637126+dogukanyildiz99@users.noreply.github.com>
2022-08-02 14:43:35 +0300

    Update diceRoll.kt
    added colour attribute to the dice class

--- diceRoll.kt
+++ diceRoll.kt
@@ -1,14 +1,13 @@
 fun main() {
-	val myFirstDice = Dice(6)
-    	println("Your ${myFirstDice.numSides} sided dice rolled ${myFirstDice.roll()}!")
+	val myFirstDice = Dice(6,"Red")
+    	println("Your ${myFirstDice.numSides} sided ${myFirstDice.diceColour} coloured dice rolled ${myFirstDice.roll()}!")
     
-    	val mySecondDice = Dice(20)
-    	println("Your ${mySecondDice.numSides} sided dice rolled ${mySecondDice.roll()}!")
+    	val mySecondDice = Dice(20,"Blue")
+    	println("Your ${mySecondDice.numSides} sided ${mySecondDice.diceColour} coloured dice rolled ${mySecondDice.roll()}!")
 }
 
 //creating a class
-class Dice(val numSides: Int) {
-    
+class Dice(val numSides: Int, val diceColour: String) {
     fun roll(): Int {
         return (1..numSides).random()
     }
