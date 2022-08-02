34cc590917d30246436a485cd9d267d63a390fac
Doğukan YILDIZ <56637126+dogukanyildiz99@users.noreply.github.com>
2022-08-02 14:33:47 +0300

    Update diceRoll.kt
    refactored the code

--- diceRoll.kt
+++ diceRoll.kt
@@ -1,7 +1,7 @@
 fun main() {
 	val myFirstDice = Dice(6)
-    	val diceRoll = myFirstDice.roll()
-    	println("Your ${myFirstDice.numSides} sided dice rolled ${diceRoll}!")
+    	println("Your ${myFirstDice.numSides} sided dice rolled ${myFirstDice.roll()}!")
+    
     	val mySecondDice = Dice(20)
     	println("Your ${mySecondDice.numSides} sided dice rolled ${mySecondDice.roll()}!")
 }
@@ -10,8 +10,7 @@ fun main() {
 class Dice(val numSides: Int) {
     
     fun roll(): Int {
-        val randomNumber = (1..numSides).random()
-        return randomNumber
+        return (1..numSides).random()
     }
 }
 
