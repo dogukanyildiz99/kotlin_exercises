4ed6cecbf5762e380fff6d23a0269f776c26c687
Doğukan YILDIZ <56637126+dogukanyildiz99@users.noreply.github.com>
2022-08-02 13:22:40 +0300

    Update randomNum.kt

--- randomNum.kt
+++ randomNum.kt
@@ -2,13 +2,13 @@ fun main() {
   	//to specify the range we're working on
 	val diceRange = 1..6
 	//val diceRange: IntRange = 1..6 is another way of specifying it
-    val randomNumber = diceRange.random()
-    //(1..6).random() is also another way of randomly generating a number between given range
-    println("Random number: ${randomNumber}")
-    /*Output
-    Random number: 5
-    Random number: 2
-    Random number: 4
-    ...
-    */
+	val randomNumber = diceRange.random()
+    	//(1..6).random() is also another way of randomly generating a number between given range
+    	println("Random number: ${randomNumber}")
+    	/*Output
+    	Random number: 5
+    	Random number: 2
+    	Random number: 4
+    	...
+    	*/
 }
