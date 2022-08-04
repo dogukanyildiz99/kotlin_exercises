3072d4b992d5bd51e505588dd3281ccad9120c3e
Doğukan YILDIZ <56637126+dogukanyildiz99@users.noreply.github.com>
2022-08-04 13:59:18 +0300

    Create ifElseWhen.kt

---
+++ ifElseWhen.kt
@@ -0,0 +1,40 @@
+fun main() {
+    val myFirstDice = Dice(6)
+    val rollResult = myFirstDice.roll()
+    val luckyNumber = 5
+    /* if, else if, else statements
+    if (rollResult == luckyNumber) {
+        println("You win!")
+    } else if (rollResult == 1) {
+        println("So sorry! You rolled a 1. Try again!")
+    } else if (rollResult == 2) {
+        println("Sadly, you rolled a 2. Try again!")
+    } else if (rollResult == 3) {
+        println("Unfortunately, you rolled a 3. Try again!")
+    } else if (rollResult == 5) {
+        println("Don't cry! You rolled a 5. Try again!")
+    } else {
+        println("Apologies! You rolled a 6. Try again!")
+    }
+    */
+    //using when instead of if statements
+    when (rollResult){
+        luckyNumber -> println("You win!")
+        1 -> println("So sorry! You rolled a 1. Try again!")
+        2 -> println("Sadly, you rolled a 2. Try again!")
+        3 -> println("Unfortunately, you rolled a 3. Try again!")
+        4 -> println("Don't cry! You rolled a 4. Try again!")
+        6 -> println("Apologies! You rolled a 6. Try again!")
+    }
+}
+
+class Dice (val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
+
+/* Possible output
+Unfortunately, you rolled a 3. Try again!
+*/
