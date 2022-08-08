4da27f4c6f496bb07cd392dc32c71126eca22127
Doğukan YILDIZ <56637126+dogukanyildiz99@users.noreply.github.com>
2022-08-08 18:20:05 +0300

    Update classes.kt
    Added carpet length function

--- classes.kt
+++ classes.kt
@@ -1,6 +1,8 @@
 // Classes and inheritance in Kotlin
 // To use pi for the round hut floor area
 import kotlin.math.PI
+// To use square root function for carpet length
+import kotlin.math.sqrt
 
 fun main() {
     val squareCabin = SquareCabin(6, 50.0)
@@ -19,15 +21,21 @@ fun main() {
     	println("Capacity: ${capacity}")
     	println("Has room? ${hasRoom()}")
         println("Floor area: ${floorArea()}")
+        println("Carpet Length: ${calculateMaxCarpetLength()}")
 	}
     // A round tower with 3 floors
-     val roundTower = RoundTower(4, 15.5, 3)
+     val roundTower = RoundTower(7, 15.5)
      with(roundTower) {
     	println("\nRound Tower\n==========")
     	println("Material: ${buildingMaterial}")
     	println("Capacity: ${capacity}")
     	println("Has room? ${hasRoom()}")
         println("Floor area: ${floorArea()}")
+        println("Has room? ${hasRoom()}")
+		getRoom()
+		println("Has room? ${hasRoom()}")
+		getRoom()
+        println("Carpet Length: ${calculateMaxCarpetLength()}")
 	}
 }
 
@@ -41,6 +49,15 @@ abstract class Dwelling(private var residents: Int) {
     }
     
     abstract fun floorArea(): Double
+    // if avalaible gives room to the resident
+    fun getRoom() {
+        if (capacity > residents) {
+            residents++
+            println("You got a room!")
+        } else {
+            println("Sorry, at capacity and no rooms left.")
+        }
+    }
 }
 
 // A subclass/child of an abstract class
@@ -62,6 +79,11 @@ open class RoundHut(residents: Int, val radius: Double) : Dwelling(residents) {
     override fun floorArea(): Double {
         return PI * radius * radius
     }
+    
+    fun calculateMaxCarpetLength(): Double {
+        // lenght of a square carpet that can be fit in a circle
+    	return sqrt(2.0) * radius
+	}
 }
 // A subclass that inherits RoundHut
 class RoundTower(residents: Int, radius: Double, val floors: Int = 2) : RoundHut(residents, radius) {
@@ -87,11 +109,17 @@ Material: Straw
 Capacity: 4
 Has room? true
 Floor area: 314.1592653589793
+Carpet Length: 14.142135623730951
 
 Round Tower
 ==========
 Material: Stone
-Capacity: 12
+Capacity: 8
 Has room? true
-Floor area: 2264.3029050748437
+Floor area: 1509.5352700498956
+Has room? true
+You got a room!
+Has room? false
+Sorry, at capacity and no rooms left.
+Carpet Length: 21.920310216782976
 */
